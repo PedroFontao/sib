ffee97353d990efbdfee5577f6c22805953b3f92
Pedro Fontão <pedrofontao16@gmail.com>
2026-09-21 11:10:43 +0100

    Adding my co-authorship to the package

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -6,6 +6,11 @@ __maintainer__ = "Diogo Cachetas"
 __email__ = "diogo.cachetas@inl.int"
 
 
-
+__author__ = "Pedro Fontão" 
+__credits__ = ["Pedro Fontão"]
+__license__ = "Apache License 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Pedro Fontão"
+__email__ = "pedrofontao812004@gmail.com"
 
 
